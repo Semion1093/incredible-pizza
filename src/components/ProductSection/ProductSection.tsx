9fb6ab3cf43b75bbbeca04dd7950934794540205
Sergey Safronov <s.safronov@korda-group.ru>
2023-08-27 14:48:22 +0200

    Add ProductLabel

--- src/components/ProductSection/ProductSection.tsx
+++ src/components/ProductSection/ProductSection.tsx
@@ -2,16 +2,18 @@ import './ProductSection.scss';
 import { ProductCard } from './../ProductCard/ProductCard';
 import React from 'react';
 
-export const ProductSection = () => {
+interface ProductSectionProps {}
+
+export const ProductSection = (props: ProductSectionProps) => {
   return (
     <div className="product-section">
       <h2>Пицца</h2>
       <div className="product-section-content">
+        <ProductCard labelText="НОВИНКА" />
         <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard labelText="ХИТ" />
+        <ProductCard labelText="ПУШКА" />
+        <ProductCard labelText="ОТВАЛ ВСЕГО" />
         <ProductCard />
         <ProductCard />
         <ProductCard />
