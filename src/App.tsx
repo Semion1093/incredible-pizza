7176d99b5d82e78029b4381f47f1ce74c7471113
Semion Yakovlev <93671427+Semion1093@users.noreply.github.com>
2023-08-27 19:07:08 +0600

    Revert "Merge pull request #6 from Semion1093/dev-7"
    This reverts commit 3cd939eb01421e8efdb657e2893932ec20959a67.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.scss';
-import { HomePage } from './pages/HomePage/HomePage';
 import React from 'react';
+import logo from './logo.svg';
 
 export const App = () => {
   return (
-    <>
-      <HomePage />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 };
--- src/components/Delimiter/Delimiter.tsx
+++
@@ -1,6 +0,0 @@
-import './Delimiter.scss';
-import React from 'react';
-
-export const Delimiter = () => {
-  return <div className="delimiter"></div>;
-};
