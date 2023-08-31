f42dea8585216b8ef9eb898bf33c3672ff744172
VladM <VIStrelnikov94@gmail.com>
2023-08-31 05:37:23 +0300

    add props

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,8 +2,35 @@ import './HomePage.scss';
 import { Delimiter } from '../../components/Delimiter/Delimiter';
 import { Header } from '../../components/Header/Header';
 import { NavigationLinks } from '../../components/NavigationLinks/NavigationLinks';
-import { PizzaSettings } from './components/PizzaSettings/PizzaSettings';
+import {
+  PizzaProps,
+  PizzaSettings,
+  toppingBacon,
+  toppingChampignons,
+  toppingCucumber,
+  toppingMozzarella,
+  toppingPepperoni,
+  toppingTomatoSauce,
+} from './components/PizzaSettings/PizzaSettings';
+import PictureEasyPeasychicken from './components/PizzaSettings/assets/EasyPeasychicken.png';
+import PictureSweetChiliChicken from './components/PizzaSettings/assets/SweetChiliChicken.png';
 import React from 'react';
+
+const SweetChiliChicken: PizzaProps = {
+  id: 1,
+  name: 'SweetChiliChicken',
+  price: 400,
+  picture: PictureSweetChiliChicken,
+  defaultToppings: [toppingMozzarella, toppingCucumber, toppingPepperoni, toppingTomatoSauce],
+};
+
+const EasyPeasychicken: PizzaProps = {
+  id: 2,
+  name: 'EasyPeasychicken',
+  price: 500,
+  picture: PictureEasyPeasychicken,
+  defaultToppings: [toppingBacon, toppingCucumber, toppingChampignons, toppingTomatoSauce],
+};
 
 export const HomePage = () => {
   return (
@@ -11,7 +38,7 @@ export const HomePage = () => {
       <Header />
       <Delimiter />
       <NavigationLinks show={false} />
-      <PizzaSettings name={'sds'} />
+      <PizzaSettings {...EasyPeasychicken} />
     </>
   );
 };
--- src/pages/HomePage/components/PizzaSettings/PizzaSettings.tsx
+++ src/pages/HomePage/components/PizzaSettings/PizzaSettings.tsx
@@ -1,10 +1,107 @@
-import React from 'react';
 import './PizzaSettings.scss';
-import { PizzaComponents } from './PizzaComponents/PizzaComponents';
+import { PizzaComponents, Topping } from './PizzaComponents/PizzaComponents';
+import Bacon from './PizzaComponents/assets/Bacon.svg';
+import Champignons from './PizzaComponents/assets/Champignons.svg';
+import Cheddar from './PizzaComponents/assets/Cheddar.svg';
+import Cucumber from './PizzaComponents/assets/Cucumber.svg';
+import Jalapeno from './PizzaComponents/assets/Jalapeno.svg';
+import Mozzarella from './PizzaComponents/assets/Mozzarella.svg';
+import Olives from './PizzaComponents/assets/Olives.svg';
+import Pepperoni from './PizzaComponents/assets/Pepperoni.svg';
+import React from 'react';
+import RedOnion from './PizzaComponents/assets/RedOnion.svg';
+import SweetPepper from './PizzaComponents/assets/SweetPepper.svg';
+import TomatoSauce from './PizzaComponents/assets/TomatoSauce.svg';
 
-interface PizzaProps {
-  name: string;
+import { ReactComponent as Info } from './assets/Info.svg';
+import { ReactComponent as Top } from './assets/Top.svg';
+
+export interface PizzaProps {
+  id?: number;
+  name?: string;
+  price?: number;
+  picture?: string;
+  defaultToppings: Topping[];
 }
+
+export const toppingMozzarella: Topping = {
+  id: 1,
+  name: 'Моцарелла',
+  price: 11,
+  iconURL: Mozzarella,
+};
+
+export const toppingCucumber: Topping = {
+  id: 2,
+  name: 'Огурцы маринованные',
+  price: 22,
+  iconURL: Cucumber,
+};
+
+export const toppingPepperoni: Topping = {
+  id: 3,
+  name: 'Пепперони',
+  price: 33,
+  iconURL: Pepperoni,
+};
+
+export const toppingTomatoSauce: Topping = {
+  id: 4,
+  name: 'Томатный соус',
+  price: 44,
+  iconURL: TomatoSauce,
+};
+
+export const toppingChampignons: Topping = {
+  id: 5,
+  name: 'Шампиньоны',
+  price: 55,
+  iconURL: Champignons,
+};
+
+export const toppingRedOnion: Topping = {
+  id: 6,
+  name: 'Красный лук',
+  price: 66,
+  iconURL: RedOnion,
+};
+
+export const toppingSweetPepper: Topping = {
+  id: 7,
+  name: 'Сладкий перец',
+  price: 77,
+  iconURL: SweetPepper,
+};
+
+export const toppingBacon: Topping = {
+  id: 8,
+  name: 'Bacon',
+  price: 88,
+  iconURL: Bacon,
+};
+
+export const toppingCheddar: Topping = {
+  id: 9,
+  name: 'Cheddar',
+  price: 99,
+  iconURL: Cheddar,
+};
+
+export const toppingJalapeno: Topping = {
+  id: 10,
+  name: 'Jalapeno',
+  price: 100,
+  iconURL: Jalapeno,
+};
+
+export const toppingOlives: Topping = {
+  id: 11,
+  name: 'Olives',
+  price: 110,
+  iconURL: Olives,
+};
+
+const additionalToppings: Topping[] = [toppingChampignons, toppingBacon, toppingRedOnion, toppingSweetPepper];
 
 export const PizzaSettings = (props: PizzaProps) => {
   return (
@@ -17,40 +114,28 @@ export const PizzaSettings = (props: PizzaProps) => {
           <p>NEW</p>
         </div>
         <div className="content">
-          <img src="./assets/Rectangle 4.png" alt="" className="pizza-img" />
+          <img src={props.picture} alt="" className="pizza-img" />
         </div>
       </div>
       <div className="right-side">
         <div className="title">
           <div className="content">
-            <img src="./assets/Group 96.svg" alt="" />
-            <h4>Пепперони по-деревенски</h4>
+            <Top />
+            <h4>{props.name}</h4>
           </div>
           <details className="button-info">
             <summary>
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                <path
-                  d="M10 0C4.47254 0 0 4.47293 0 10C0 15.5275 4.47293 20 10 20C15.5275 20 20 15.5271 20 10C20 4.47254 15.527 0 10 0ZM10 18.4375C5.33621 18.4375 1.5625 14.6635 1.5625 10C1.5625 5.33621 5.33652 1.5625 10 1.5625C14.6638 1.5625 18.4375 5.33652 18.4375 10C18.4375 14.6638 14.6634 18.4375 10 18.4375Z"
-                  fill="black"
-                />
-                <path
-                  d="M10 8.37227C9.56852 8.37227 9.21875 8.72203 9.21875 9.15352V14.1845C9.21875 14.616 9.56852 14.9657 10 14.9657C10.4315 14.9657 10.7812 14.6159 10.7812 14.1845V9.15352C10.7812 8.72203 10.4315 8.37227 10 8.37227Z"
-                  fill="black"
-                />
-                <path
-                  d="M10 7.41562C10.5825 7.41562 11.0547 6.94342 11.0547 6.36093C11.0547 5.77844 10.5825 5.30624 10 5.30624C9.41751 5.30624 8.94531 5.77844 8.94531 6.36093C8.94531 6.94342 9.41751 7.41562 10 7.41562Z"
-                  fill="black"
-                />
-              </svg>
+              <Info />
             </summary>
-            <p>Это вкусно, но не то чтобы по вкусу вкусно, но по сути вкусно.</p>
+            <p>
+              В конструкторе пиццы вы можете исключить ингридиенты, которые идут в базовой конфигурации и добавить за дополнительную плату топпинги
+            </p>
           </details>
         </div>
         <div className="components-section">
-          <PizzaComponents name={'Моцарелла'} />
-          <PizzaComponents name={'Огурцы маринованные'} />
-          <PizzaComponents name={'Пепперони'} />
-          <PizzaComponents name={'Томатный соус'} />
+          {props.defaultToppings.map((item) => (
+            <PizzaComponents key={`id-${item.id}`} iconURL={item.iconURL} name={item.name} />
+          ))}
         </div>
         <div className="base-section">
           <div className="dough-wrapper">
@@ -59,6 +144,21 @@ export const PizzaSettings = (props: PizzaProps) => {
           <div className="size-wrapper">
             <div className="size"></div>
           </div>
+        </div>
+        <div className="content">
+          <h4>Добавьте в пиццу</h4>
+        </div>
+        <div className="components-section">
+          {additionalToppings.map((item) => (
+            <PizzaComponents key={`id-${item.id}`} iconURL={item.iconURL} name={item.name} />
+          ))}
+        </div>
+        <div className="results">
+          <span>
+            Итого: {props.price}
+            <p>{props.price}</p>
+          </span>
+          <button className="add-to-cart">Выбрать</button>
         </div>
       </div>
     </article>
