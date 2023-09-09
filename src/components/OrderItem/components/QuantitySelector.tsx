b1a2e0088e555f2118dbd5efec0cce3bb27143f3
vladislav.b <vladislav.b@ati.su>
2023-09-10 01:21:18 +0300

    Добавил UseEffect и поправил имена свойств

--- src/components/OrderItem/components/QuantitySelector.tsx
+++ src/components/OrderItem/components/QuantitySelector.tsx
@@ -28,7 +28,7 @@ export const QuantitySelector = () => {
       <input type="text" value={index} />
       <button className="increase" onClick={increase}>
         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
-          <g clip-path="url(#clip0_236_25160)">
+          <g clipPath="url(#clip0_236_25160)">
             <path
               d="M8 4C8 3.58579 7.66421 3.25 7.25 3.25H0.75C0.335786 3.25 0 3.58579 0 4C0 4.41421 0.335786 4.75 0.75 4.75H7.25C7.66421 4.75 8 4.41421 8 4Z"
               fill="#FF7010"
--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -4,64 +4,72 @@ import { Delimiter } from '../../components/Delimiter/Delimiter';
 import { Footer } from '../../components/Footer/Footer';
 import { Header } from '../../components/Header/Header';
 import { NavigationLinks } from '../../components/NavigationLinks/NavigationLinks';
+import { useEffect, useState } from 'react';
 import SwitchSelector from './assets/Табы.png';
 
-const orders: Order[] = [
-  {
-    number: 130312,
-    date: '22.06.21',
-    price: 1197,
-    status: 'Обрабатывается',
-    paid: 'Картой',
-    address: 'ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#',
-    items: [
-      {
-        id: 1,
-        name: 'Пепперони по-деревенски',
-        description: 'Традиционное тесто, 23 см',
-        quantity: 1,
-        cost: '399 ₽',
-        img: 'url',
-      },
-      {
-        id: 2,
-        name: 'Пепперони по-деревенски',
-        description: 'Традиционное тесто, 23 см',
-        quantity: 1,
-        cost: '399 ₽',
-        img: 'url',
-      },
-      {
-        id: 3,
-        name: 'Пепперони по-деревенски',
-        description: 'Традиционное тесто, 23 см',
-        quantity: 1,
-        cost: '399 ₽',
-        img: 'url',
-      },
-    ],
-  },
-  {
-    number: 130313,
-    date: '22.06.21',
-    price: 399,
-    status: 'Обрабатывается',
-    paid: 'Картой',
-    address: 'ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#',
-    items: [
-      {
-        id: 1,
-        name: 'Пепперони по-деревенски',
-        description: 'Традиционное тесто, 23 см',
-        quantity: 1,
-        cost: '399 ₽',
-        img: 'url',
-      },
-    ],
-  },
-];
+// const orders: Order[] = [
+//   {
+//     number: 130312,
+//     date: '22.06.21',
+//     price: 1197,
+//     status: 'Обрабатывается',
+//     paid: 'Картой',
+//     address: 'ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#',
+//     items: [
+//       {
+//         id: 1,
+//         name: 'Пепперони по-деревенски',
+//         description: 'Традиционное тесто, 23 см',
+//         quantity: 1,
+//         cost: '399 ₽',
+//         img: 'url',
+//       },
+//       {
+//         id: 2,
+//         name: 'Пепперони по-деревенски',
+//         description: 'Традиционное тесто, 23 см',
+//         quantity: 1,
+//         cost: '399 ₽',
+//         img: 'url',
+//       },
+//       {
+//         id: 3,
+//         name: 'Пепперони по-деревенски',
+//         description: 'Традиционное тесто, 23 см',
+//         quantity: 1,
+//         cost: '399 ₽',
+//         img: 'url',
+//       },
+//     ],
+//   },
+//   {
+//     number: 130313,
+//     date: '22.06.21',
+//     price: 399,
+//     status: 'Обрабатывается',
+//     paid: 'Картой',
+//     address: 'ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#',
+//     items: [
+//       {
+//         id: 1,
+//         name: 'Пепперони по-деревенски',
+//         description: 'Традиционное тесто, 23 см',
+//         quantity: 1,
+//         cost: '399 ₽',
+//         img: 'url',
+//       },
+//     ],
+//   },
+// ];
 
 export const AccountPage = () => {
+  const [orders, setAccountOrders] = useState<Order[]>([]);
+  useEffect(() => {
+    fetch('https://b9b7088f-832b-475f-8c8b-c404b476a9c2.mock.pstmn.io/account-orders')
+      .then((response) => response.json())
+      .then((json) => setAccountOrders(json));
+  }, []);
+
   return (
     <>
       <Header />
