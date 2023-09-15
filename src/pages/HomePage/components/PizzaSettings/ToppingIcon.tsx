abd84607a27cbf9a80840576ff4f050fcd283baf
VladM <VIStrelnikov94@gmail.com>
2023-09-15 05:41:14 +0300

    added ToppingIcon

---
+++ src/pages/HomePage/components/PizzaSettings/ToppingIcon.tsx
@@ -0,0 +1,46 @@
+import { ReactComponent as Bacon } from './PizzaComponents/assets/Bacon.svg';
+import { ReactComponent as Champignons } from './PizzaComponents/assets/Champignons.svg';
+import { ReactComponent as Cheddar } from './PizzaComponents/assets/Cheddar.svg';
+import { ReactComponent as Cucumber } from './PizzaComponents/assets/Cucumber.svg';
+import { ReactComponent as Jalapeno } from './PizzaComponents/assets/Jalapeno.svg';
+import { ReactComponent as Mozzarella } from './PizzaComponents/assets/Mozzarella.svg';
+import { ReactComponent as Olives } from './PizzaComponents/assets/Olives.svg';
+import { ReactComponent as Pepperoni } from './PizzaComponents/assets/Pepperoni.svg';
+import { ReactComponent as RedOnion } from './PizzaComponents/assets/RedOnion.svg';
+import { ReactComponent as SweetPepper } from './PizzaComponents/assets/SweetPepper.svg';
+import { ReactComponent as TomatoSauce } from './PizzaComponents/assets/TomatoSauce.svg';
+import React from 'react';
+
+export enum Topping {
+  Mozzarella = 'Mozzarella',
+  Cucumber = 'Cucumber',
+  Pepperoni = 'Pepperoni',
+  TomatoSauce = 'TomatoSauce',
+  Champignons = 'Champignons',
+  RedOnion = 'RedOnion',
+  Bacon = 'Bacon',
+  SweetPepper = 'SweetPepper',
+  Cheddar = 'Cheddar',
+  Jalapeno = 'Jalapeno',
+  Olives = 'Olives',
+}
+
+interface ToppingIconProps {
+  icon: Topping;
+}
+
+export const ToppingIcon = (props: ToppingIconProps) => (
+  <>
+    {props.icon === Topping.Mozzarella && <Mozzarella />}
+    {props.icon === Topping.Cucumber && <Cucumber />}
+    {props.icon === Topping.Pepperoni && <Pepperoni />}
+    {props.icon === Topping.TomatoSauce && <TomatoSauce />}
+    {props.icon === Topping.Champignons && <Champignons />}
+    {props.icon === Topping.RedOnion && <RedOnion />}
+    {props.icon === Topping.Bacon && <Bacon />}
+    {props.icon === Topping.SweetPepper && <SweetPepper />}
+    {props.icon === Topping.Cheddar && <Cheddar />}
+    {props.icon === Topping.Jalapeno && <Jalapeno />}
+    {props.icon === Topping.Olives && <Olives />}
+  </>
+);
