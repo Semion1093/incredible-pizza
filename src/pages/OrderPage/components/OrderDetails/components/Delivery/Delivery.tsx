146e23275b790f8bb3819e75e860239d14cd3fdd
VladM <VIStrelnikov94@gmail.com>
2023-09-16 16:53:01 +0300

    relocated SwitchSelector

--- src/pages/OrderPage/components/OrderDetails/components/Delivery/Delivery.tsx
+++ src/pages/OrderPage/components/OrderDetails/components/Delivery/Delivery.tsx
@@ -1,15 +1,9 @@
 import './Delivery.scss';
 import { DeliveryAddress } from './components/DeliveryAddress/DeliveryAddress';
 import { DeliverySwitchSelector } from './components/DeliverySwitchSelector/DeliverySwitchSelector';
-import { Options } from '../../../../../../components/OrderItem/SwitchSelector/SwitchSelector';
 import { OrderDeliveryTime } from './components/OrderDeliveryTime/OrderDeliveryTime';
 import { PickupAddress } from './components/PickupAddress/PickupAddress';
 import React, { useState } from 'react';
-
-const options: Options[] = [
-  { value: 'delivery', label: 'Доставка' },
-  { value: 'pickup', label: 'Самовывоз' },
-];
 
 export const Delivery = () => {
   const [deliveryTab, setDeliveryTab] = useState('delivery');
