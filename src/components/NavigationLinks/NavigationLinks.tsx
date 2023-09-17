9b0d14d5f264f14e98a7ec2aa88a8329054056c1
Sergey Safronov <s.safronov@korda-group.ru>
2023-09-18 00:35:29 +0200

    Some fixes for PR to main

--- src/components/NavigationLinks/NavigationLinks.tsx
+++ src/components/NavigationLinks/NavigationLinks.tsx
@@ -8,7 +8,6 @@ import { NavigationLinksTextSvg } from './assets/NavigationLinksText';
 import React from 'react';
 
 interface NavigationLinksProps {
-  showSections: boolean;
   showMobileButtonBack: boolean;
 }
 
@@ -23,18 +22,16 @@ export const NavigationLinks = (props: NavigationLinksProps) => {
             <NavigationLinksTextSvg />
           </Link>
         </div>
-        {props.showSections && (
-          <nav className="desktop-only">
-            <a href="#promo">Акции</a>
-            <a href="#pizza">Пицца</a>
-            <a href="#sushi">Суши</a>
-            <a href="#combo">Комбо</a>
-            <a href="#snack">Закуски</a>
-            <a href="#drink">Напитки</a>
-            <a href="#dessert">Десерты</a>
-            <a href="#sauce">Соусы</a>
-          </nav>
-        )}
+        <nav className="desktop-only">
+          <a href="#promo">Акции</a>
+          <a href="#pizza">Пицца</a>
+          <a href="#sushi">Суши</a>
+          <a href="#combo">Комбо</a>
+          <a href="#snack">Закуски</a>
+          <a href="#drink">Напитки</a>
+          <a href="#dessert">Десерты</a>
+          <a href="#sauce">Соусы</a>
+        </nav>
         <button className="desktop-only">
           <NavigationLinksBasketImageSvg /> 0 ₽
         </button>
--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -4,27 +4,6 @@ import { Delimiter } from '../../components/Delimiter/Delimiter';
 import { useEffect, useState } from 'react';
 import SwitchSelector from './assets/Табы.png';
 
-// const orders: Order[] = [
-//   {
-//     number: 130312,
-//     date: '22.06.21',
-//     price: 1197,
-//     status: 0,
-//     eta: undefined,
-//     paid: 'Картой',
-//     address: 'ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#',
-//   },
-//   {
-//     number: 130313,
-//     date: '22.06.21',
-//     price: 399,
-//     status: 1,
-//     eta: '15:13',
-//     paid: 'Наличкой',
-//     address: 'ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#',
-//   },
-// ];
-
 export const AccountPage = () => {
   const [orders, setAccountOrders] = useState<Order[]>([]);
   useEffect(() => {
