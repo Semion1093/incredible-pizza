3c887042d2da0ec1b230230f4c53247e2c75680f
vladislav.b <vladislav.b@ati.su>
2023-09-17 16:41:17 +0300

    Заменил болванку на AccountPage на SwitchSelector

--- src/pages/AccountPage/AccountPage.tsx
+++ src/pages/AccountPage/AccountPage.tsx
@@ -1,31 +1,11 @@
 import './AccountPage.scss';
 import { AccountOrder, Order } from './components/AccountOrder/AccountOrder';
 import { Delimiter } from '../../components/Delimiter/Delimiter';
+import { SwitchSelector } from '../../components/SwitchSelector/SwitchSelector';
 import { useEffect, useState } from 'react';
-import SwitchSelector from './assets/Табы.png';
-
-// const orders: Order[] = [
-//   {
-//     number: 130312,
-//     date: '22.06.21',
-//     price: 1197,
-//     status: 0,
-//     eta: undefined,
-//     paid: 'Картой',
-//     address: 'ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#',
-//   },
-//   {
-//     number: 130313,
-//     date: '22.06.21',
-//     price: 399,
-//     status: 1,
-//     eta: '15:13',
-//     paid: 'Наличкой',
-//     address: 'ул. Львовская 48/2, офис 301, 2 этаж, домофон 4801#',
-//   },
-// ];
 
 export const AccountPage = () => {
+  const [deliveryTab, setDeliveryTab] = useState('leftOption');
   const [orders, setAccountOrders] = useState<Order[]>([]);
   useEffect(() => {
     fetch('https://incredible-pizza.free.beeceptor.com/account-orders')
@@ -38,7 +18,7 @@ export const AccountPage = () => {
       <div className="account-page">
         <div className="header">
           <h1>Мой аккаунт</h1>
-          <img src={SwitchSelector} alt="tabs" />
+          <SwitchSelector clickHandler={setDeliveryTab} leftOptionText="История заказов" rightOptionText="Настройки" />
         </div>
         <div className="orders">
           {orders.map((order) => (
