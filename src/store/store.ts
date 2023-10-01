c41d83690de48b2efd844b23f216efea94ca189b
vladislavstr <58762058+vladislavstr@users.noreply.github.com>
2023-10-01 20:02:50 +0300

    updated store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,27 +1,43 @@
-import { cartPageReducer } from '../pages/HomePage/components/ProductCard/productCartSlice';
-import { configureStore } from '@reduxjs/toolkit';
-import { userAccountModalReducer } from '../components/UserAccountModal/userAccountModalSlice';
-import { pizzaSettingsReducer } from '../pages/HomePage/components/PizzaSettings/pizzaSettingsSlice';
-import { authReducer } from './authSlice';
-import { currentUserReducer } from './currentUserSlice';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
 import { authModalReducer } from '../pages/HomePage/components/AuthModal/authModalSlice';
+import { authReducer } from './authSlice';
+import { cartPageReducer } from '../pages/HomePage/components/ProductCard/productCartSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { currentUserReducer } from './currentUserSlice';
+import { pizzaSettingsReducer } from '../pages/HomePage/components/PizzaSettings/pizzaSettingsSlice';
 import { signInReducer } from '../pages/HomePage/components/AuthModal/SignInSignUp/signInSlice';
 import { signUpReducer } from '../pages/HomePage/components/AuthModal/SignInSignUp/signUpSlice';
+import { userAccountModalReducer } from '../components/UserAccountModal/userAccountModalSlice';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-  reducer: {
-    currentUser: currentUserReducer,
-    authUser: authReducer,
-    cartPage: cartPageReducer,
-    authModal: authModalReducer,
-    signInModal: signInReducer,
-    signUpModal: signUpReducer,
-    userAccountModal: userAccountModalReducer,
-    pizzaSettingsModal: pizzaSettingsReducer,
-  },
+const rootReducer = combineReducers({
+  currentUser: currentUserReducer,
+  cartPage: cartPageReducer,
+  authModal: authModalReducer,
+  signInModal: signInReducer,
+  signUpModal: signUpReducer,
+  userAccountModal: userAccountModalReducer,
+  pizzaSettingsModal: pizzaSettingsReducer,
+  authUser: authReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+const persistConfig = {
+  key: 'products',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
